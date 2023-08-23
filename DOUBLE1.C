ec865de8ab9f8d0d065e4a0f73490ff7694e7f4c
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-08-23 18:41:27 +0530

    Add files via upload

---
+++ DOUBLE1.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+double a,b;
+double sum;
+clrscr();
+printf("Enter A:");
+scanf("%lf",&a);
+printf("Enter B:");
+scanf("%lf",&b);
+sum=a*b;
+printf("\nThe Multiplication of A and B is: %lf",sum);
+}
