30cb4ac84cd0672ee532296596ae81dc688e8f64
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-08-23 14:14:06 +0530

    Add files via upload

---
+++ ADD1.C
@@ -0,0 +1,14 @@
+
+#include  <stdio.h>
+void main()
+{
+ int a,b,sum;
+ clrscr();
+ //a=5;
+ printf("Enter A:");
+ scanf("%d",&a);
+ printf("Enter B:");
+ scanf("%d",&b);
+ sum=a+b;
+ printf("The sum is: %d",sum);
+ }
