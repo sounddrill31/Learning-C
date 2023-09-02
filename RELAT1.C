d13b02017aa82b620025999df6cb9271e4cf883c
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-09-01 22:11:12 -0700

    Add files via upload

---
+++ RELAT1.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a;
+int b;
+int stat;
+printf("We are going to take inputs for the first and second values. Enter comparison to be performed: \n1. Less Than\n2. Greater than\n3. Less than or Is Equal to\n4. Greater than or Is Equal to\n5. Is Equal to\n6. Not Equal To\nEnter your selection (1,2,3,4,5,6 or 7 to exit):");
+scanf("%d"&stat);
+if
+}
