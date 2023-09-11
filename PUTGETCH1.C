ae4a50bd36a13b1591e92fae5dd661551f15267c
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-09-11 11:41:49 +0530

    Add files via upload

---
+++ PUTGETCH1.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+int main()
+{
+	//char name[10];
+	char ch;
+	clrscr();
+	puts("Enter a Character!\n");
+       ch=getchar();
+	puts("Entered Character is\n");
+	putchar(ch);
+	//puts("Enter your name!\n");
+       //gets(name);
+       //puts("Entered Name is\n");
+       //puts(name);
+	return 0;
+}
